f47756a679c218a60576fdb0297f354a42454828
Pablo M <pmartinez@bipgo.com>
2023-05-18 15:15:28 -0500

    changes delete files

--- resources/views/livewire/car/card.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="w-full flex flex-col">
-    <div class="relative">
-        <div class="w-full absolute top-0 left-0 bg-blue text-white text-center font-bold text-sm py-1">Fotos 360</div>
-        <img src="/images/car.jpg" alt=""/>
-    </div>
-    <h2 class="text-blue font-bold mt-2">3008N GTL 16E BA6 AC 1.6 5P 4X2 TA</h2>
-    <p class="text-blue">2017 I 118,851 KM I AUTOMATICA</p>
-    <p class="text-blue text-lg font-bold">$24,300</p>
-</div>
