f6dc49f15646db8576895716bb3531ce845df9cf
Pablo M <pmartinez@bipgo.com>
2023-05-19 09:42:59 -0500

    login changes

--- resources/views/cms/Components/TextInput.vue
+++ resources/views/cms/Components/TextInput.vue
@@ -3,6 +3,10 @@ import { onMounted, ref } from 'vue';
 
 defineProps({
     modelValue: String,
+    placeholder:{
+        String, 
+        default:''
+    }
 });
 
 defineEmits(['update:modelValue']);
@@ -21,8 +25,9 @@ defineExpose({ focus: () => input.value.focus() });
 <template>
     <input
         ref="input"
-        class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm"
+        class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-100 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
         :value="modelValue"
+        :placeholder="placeholder"
         @input="$emit('update:modelValue', $event.target.value)"
     >
 </template>
